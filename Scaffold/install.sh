b4197590b1708f2f6a8aa4bd2fc1c4d0f4c2e083
ferrerax <quimfersa@hotmail.com>
2019-11-23 20:55:09 +0100

    dhcp fet

--- Scaffold/install.sh
+++ Scaffold/install.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
+#### FUNCIONS
 SetDockerRepository() {
-	sudo apt-get update -y
-	sudo apt-get install -y \
-    	apt-transport-https \
+	 sudo apt-get install -y \
+    	 apt-transport-https \
    	 ca-certificates \
    	 curl \
    	 gnupg-agent \
@@ -28,6 +28,18 @@ SetDockerRepository() {
 	return 0;
 }
 
+### SCRIPT
+
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+#ssh
+sudo apt-get install openssh-server
+sudo systemctl start ssh.service
+#end ssh
+
+#docker
+
 SetDockerRepository;
 if (( $? )); then
 	echo "Error installing the docker repositories, exiting...";
@@ -37,6 +49,21 @@ else
 	sudo apt autoremove -y;
 fi
 	
+#end docker
+
+
+#dhcp config
+iface=ip link | awk -F: '$0 !~ "lo|vir|docker*|wl|^[^0-9]"{print $2;getline}' | head -n1
+sudo ip link set dev $iface up
+sudo ip add address add 172.17.0.1/24 dev $iface
+mkdir ./data
+cp dhcpd.conf data/dhcpd.conf
+sudo docker pull networkboot/dhcpd
+docker run -it --rm --init --net host -v "$(pwd)/data":/data networkboot/dhcpd $iface
+# end dhcp config
+
+#config dns
+
 
 
 
